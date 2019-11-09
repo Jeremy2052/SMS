a1825fdff0e605aa75dcbd6e3a633227f5ac6db7
Jeremy Evans <jeremy.evans@mavs.uta.edu>
2019-11-09 10:57:41 -0800

    update registration screen

--- app/src/main/java/com/example/login/registration.java
+++ app/src/main/java/com/example/login/registration.java
@@ -3,6 +3,7 @@ package com.example.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class registration extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class registration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        final EditText firstName = (findViewById(R.id.first));
+        final EditText lastName = (findViewById(R.id.last));
+        final EditText email = (findViewById(R.id.email));
+        final EditText phoneNumber = (findViewById(R.id.phonenumber));
     }
 }
