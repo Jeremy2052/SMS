c4e4f667c93c0d607f61bc982bb8f68ee76f0e33
Jeremy Evans <jeremy.evans@mavs.uta.edu>
2019-11-15 11:43:36 -0800

    Update Login, Registration

--- app/src/main/java/com/example/login/registration.java
+++ app/src/main/java/com/example/login/registration.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class registration extends AppCompatActivity {
 
@@ -23,8 +25,48 @@ public class registration extends AppCompatActivity {
         final EditText password = (findViewById(R.id.passwordR));
 
         Button signup = findViewById(R.id.signup);
+
+        final String first = firstName.getText().toString();
+        final String last = lastName.getText().toString();
+        final String Email = email.getText().toString();
+        final String phone = phoneNumber.getText().toString();
+        final String user = username.getText().toString();
+        final String pass = password.getText().toString();
+
         signup.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+
+                if(TextUtils.isEmpty(first)){
+                    //first name is empty
+                    Toast.makeText(getApplication(), "Enter a first name",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(last)){
+                    //first name is empty
+                    Toast.makeText(getApplication(), "Enter a last name",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(Email)){
+                    //first name is empty
+                    Toast.makeText(getApplication(), "Enter an email",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(phone)){
+                    //first name is empty
+                    Toast.makeText(getApplication(), "Enter a phone number",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(user)){
+                    //first name is empty
+                    Toast.makeText(getApplication(), "Enter a username",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(pass)){
+                    //first name is empty
+                    Toast.makeText(getApplication(), "Enter a password",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
             }
