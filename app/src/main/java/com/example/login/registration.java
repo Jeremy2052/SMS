236c3015b27e2ee08f03879f922bd4596168b77e
Jeremy Evans <jeremy.evans@mavs.uta.edu>
2019-11-16 10:20:50 -0800

    Update registration with firebase

--- app/src/main/java/com/example/login/registration.java
+++ app/src/main/java/com/example/login/registration.java
@@ -1,5 +1,6 @@
 package com.example.login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,66 +11,102 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class registration extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class registration extends AppCompatActivity implements View.OnClickListener {
+    private EditText firstName;
+    private EditText lastName;
+    private EditText email;
+    private EditText phoneNumber;
+    private EditText username;
+    private EditText password;
+    private Button signup;
+
+    private FirebaseAuth firebaseAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        final EditText firstName = (findViewById(R.id.first));
-        final EditText lastName = (findViewById(R.id.last));
-        final EditText email = (findViewById(R.id.email));
-        final EditText phoneNumber = (findViewById(R.id.phonenumber));
-        final EditText username = (findViewById(R.id.usernameR));
-        final EditText password = (findViewById(R.id.passwordR));
+        firebaseAuth = FirebaseAuth.getInstance();
 
-        Button signup = findViewById(R.id.signup);
+        firstName = (findViewById(R.id.first));
+        lastName = (findViewById(R.id.last));
+        email = (findViewById(R.id.email));
+        phoneNumber = (findViewById(R.id.phonenumber));
+        username = (findViewById(R.id.usernameR));
+        password = (findViewById(R.id.passwordR));
+        signup = findViewById(R.id.signup);
 
-        final String first2 = firstName.getText().toString();
-        final String last2 = lastName.getText().toString();
-        final String Email = email.getText().toString();
-        final String phone = phoneNumber.getText().toString();
-        final String user = username.getText().toString();
-        final String pass = password.getText().toString();
+        signup.setOnClickListener(this);
 
-        signup.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+    }
 
-                if(TextUtils.isEmpty(first2)){
-                    //first name is empty
-                    Toast.makeText(getApplication(), "Enter a first name",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if(TextUtils.isEmpty(last2)){
-                    //first name is empty
-                    Toast.makeText(getApplication(), "Enter a last name",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if(TextUtils.isEmpty(Email)){
-                    //first name is empty
-                    Toast.makeText(getApplication(), "Enter an email",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if(TextUtils.isEmpty(phone)){
-                    //first name is empty
-                    Toast.makeText(getApplication(), "Enter a phone number",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if(TextUtils.isEmpty(user)){
-                    //first name is empty
-                    Toast.makeText(getApplication(), "Enter a username",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if(TextUtils.isEmpty(pass)){
-                    //first name is empty
-                    Toast.makeText(getApplication(), "Enter a password",Toast.LENGTH_SHORT).show();
-                    return;
-                }
+    private void register(){
+        String first2 = firstName.getText().toString().trim();
+        String last2 = lastName.getText().toString().trim();
+        String Email = email.getText().toString().trim();
+        String phone = phoneNumber.getText().toString().trim();
+        String user = username.getText().toString().trim();
+        String pass = password.getText().toString().trim();
 
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
+        if(TextUtils.isEmpty(first2)){
+            //first name is empty
+            Toast.makeText(this, "Enter a first name",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(last2)){
+            //first name is empty
+            Toast.makeText(this, "Enter a last name",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(user)){
+            //first name is empty
+            Toast.makeText(this, "Enter a username",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(pass)){
+            //first name is empty
+            Toast.makeText(this, "Enter a password",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(Email)){
+            //first name is empty
+            Toast.makeText(this, "Enter an email",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(phone)){
+            //first name is empty
+            Toast.makeText(this, "Enter a phone number",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        firebaseAuth.createUserWithEmailAndPassword(Email,pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    //display registration was successful
+                    Toast.makeText(registration.this, "Register successful",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(registration.this, "Could not register, please try again",Toast.LENGTH_SHORT).show();
+                }
             }
         });
+
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == signup){
+            register();
+        }
     }
 }
